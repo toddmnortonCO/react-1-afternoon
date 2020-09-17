1a99e9dbc11cd888586aec2c75f3cabc06f6176e
toddmnorton <toddmnorton@live.com>
2020-09-17 04:56:57 -0600

    saving changes

---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+export default class FilterObject extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            employees: [
+                {
+                    name: 'Todd',
+                    skill: 'novice',
+                    age: 35
+                },
+                {
+                    name: 'Mike',
+                    skill: 'pro',
+                    age: 23
+                },
+                {
+                    name: 'Ralph',
+                    skill: 'moderate',
+                    age: 31
+                },
+                {
+                    name: 'Harry',
+                    happiness: 'high',
+                    height: 72
+                }
+
+            ],
+            userInput: '',
+            filteredEmployees: []
+        }
+    }
+    handleChange(val) {
+        this.setState({ userInput: val });
+    }
+    
+    filteredEmployees(prop) {
+        let employees = this.state.employees;
+        let filteredEmployees = [];
+
+        for (let i = 0; i < employees.length; i++) {
+            if (employees[i].hasOwnProperty(prop)) {
+                filteredEmployees.push(employees[i]);
+            }
+        }
+        this.setState({ filteredEmployees: filteredEmployees });
+    }
+
+    render() {
+        return (
+            <div className='puzzleBox filterObjectPB'>
+                <h4>Filter Object</h4>
+                <span className='puzzleText'> Original: {JSON.stringify(this.state.employees, null, 10)}</span>
+                <input className='inputLine' onChange={ (e) => this.handleChange(e.target.value)} />
+                <button className='confirmationButton' onClick={ () => this.filterEmployees(this.state.userInput)}>Filter</button>
+                <span className='resultsBox filterObjectRB'> Filtered: {JSON.stringify(this.state.filteredEmployees, null, 10)}</span>
+            </div>
+        )
+    }
+}
